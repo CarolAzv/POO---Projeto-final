34480a207572335727d6e11c782ccba53f0e3369
Carol <caroldazevedo52@gmail.com>
2025-07-23 13:56:15 -0300

    Update Projeto.py

--- Parte 2/Modelos/Projeto.py
+++ Parte 2/Modelos/Projeto.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class Projetos:
   def __inti__(self, id, descricao, data_comeco, status, idFuncionario):
     self.set_id(id)
@@ -9,10 +10,27 @@ class Projetos:
   def set_id(self, id):
     self.__id = id
   def set_descricao(self, descricao):
+    if descricao == "":
+            raise ValueError("Informe a descrição!")
     self.__descricao = descricao
   def set_data_comeco(self, data_comeco):
+     if d == "":
+            raise ValueError("A data de começo não pode está vazia!")
     self.__data_comeco = data_comeco
   def set_status(self, status):
+    if status == "":
+             raise ValueError("Informe o status!")
     self.__status = status
   def set_idFuncionario(self, idFuncionario):
     self.__idFuncionario = idFuncionario
+
+  def get_id(self):
+    return self.__id
+  def get_descricao(self):
+    return self.__descricao
+  def get_data_comeco(self):
+    return self.__data_comeco
+  def get_status(self):
+    return self.__status
+  def get_idFuncionario(self):
+    return self.__idFuncionario
