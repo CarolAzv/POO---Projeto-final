0c7907b72cb2651555e1f0723b47c6a25ee5f661
Carol <caroldazevedo52@gmail.com>
2025-07-23 13:16:57 -0300

    Update Funcionario.py

--- Parte 2/Modelos/Funcionario.py
+++ Parte 2/Modelos/Funcionario.py
@@ -1,10 +1,10 @@
 import streamlit as st
-class Cliente:
-    def __init__(self, id, nome, email, fone, senha):
+class Funcionario:
+    def __init__(self, id, nome, email, cell, senha):
         self.__id = id
         self.set_nome(nome)
         self.set_email(email)
-        self.set_fone(fone)
+        self.set_cell(cell)
         self.set_senha(senha)
       
     def set_id(self, id):
@@ -14,24 +14,24 @@ class Cliente:
         self.__nome = nome
     def set_email(self, email):
         self.__email = email
-    def set_fone(self, fone):
-        if len(fone) == 11:
-            self.__fone = fone
+    def set_cell(self, cell):
+        if len(cell) == 11:
+            self.__cel = cel
         else: raise ValueError("O número deve está no formato xxxxxxxxxxx - com 11 números")
-    def set_senha(self,s):
+    def set_senha(self, s):
         if len(s) < 4 : raise ValueError(f"Senha deve ter no mínimo 4 caracteres")
         self.__senha = s
 
     def get_id(self): return self.__id
     def get_nome(self): return self.__nome
     def get_email(self): return self.__email
-    def get_fone(self): return self.__fone
+    def get_cell(self): return self.__cell
     def get_senha(self): return self.__senha
       
     def __str__(self):
-        return f"{self.__id} - {self.__nome} - {self.__email} - {self.__senha} - {self.__fone} - {self.__endereco}"
+        return f"{self.__id} - {self.__nome} - {self.__email} - {self.__cell} - {self.__senha}"
     def to_dict(self):
-        return {"id": self.__id,"nome": self.__nome,"email": self.__email, "senha": self.__senha, "fone": self.__fone,"endereco":self.__endereco}
+        return {"id": self.__id,"nome": self.__nome,"email": self.__email, "cell": self.__cell, "senha": self.__senha}
       
     @classmethod
     def from_dict(cls, data):
@@ -39,6 +39,6 @@ class Cliente:
             id=data['id'],
             nome=data['nome'],
             email=data['email'],
-            fone=data['fone'],
+            cell=data['cell'],
             senha=data['senha']
         )
