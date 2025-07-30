5ba0ae436166aa7ec76fa62fadbd8502794eea10
Carol <caroldazevedo52@gmail.com>
2025-07-29 23:26:13 -0300

    Update Projeto.py

--- Parte 2/Modelos/Projeto.py
+++ Parte 2/Modelos/Projeto.py
@@ -1,39 +1,54 @@
 from datetime import datetime
 class Projetos:
-  def __inti__(self, id, descricao, data_comeco, data_fim, status, idFuncionario):
-    self.set_id(id)
-    self.set_descricao(descricao)
-    self.set_data_comeco(data_comeco)
-    self.set_data_fim(data_fim)
-    self.set_status(status)
-    self.set_idFuncionario(idFuncionario)
+    def __inti__(self, id, descricao, data_comeco, data_fim, status, idPessoa):
+        self.set_id(id)
+        self.set_descricao(descricao)
+        self.set_data_comeco(data_comeco)
+        self.set_data_fim(data_fim)
+        self.set_status(status)
+        self.set_idPessoa(idPessoa)
 
-  def set_id(self, id):
-    self.__id = id
-  def set_descricao(self, descricao):
-    if descricao == "":
+    def set_id(self, id):
+        self.__id = id
+    def set_descricao(self, descricao):
+        if descricao == "":
             raise ValueError("Informe a descrição!")
-    self.__descricao = descricao
-  def set_data_comeco(self, data_comeco):
-     if d == "":
+        self.__descricao = descricao
+    def set_data_comeco(self, data_comeco):
+        if d == "":
             raise ValueError("A data de começo não pode está vazia!")
-    self.__data_comeco = data_comeco
-  def set_data_fim(self, data_fim):
-    self.__data_fim = data_fim
-  def set_status(self, status):
-    if status == "":
+        self.__data_comeco = data_comeco
+    def set_data_fim(self, data_fim):
+        self.__data_fim = data_fim
+    def set_status(self, status):
+        if status == "":
              raise ValueError("Informe o status!")
-    self.__status = status
-  def set_idFuncionario(self, idFuncionario):
-    self.__idFuncionario = idFuncionario
+        self.__status = status
+    def set_idPessoa(self, idPessoa):
+        self.__idPessoa = idPessoa
 
-  def get_id(self):
-    return self.__id
-  def get_descricao(self):
-    return self.__descricao
-  def get_data_comeco(self):
-    return self.__data_comeco
-  def get_status(self):
-    return self.__status
-  def get_idFuncionario(self):
-    return self.__idFuncionario
+    def get_id(self):
+        return self.__id
+    def get_descricao(self):
+        return self.__descricao
+    def get_data_comeco(self):
+        return self.__data_comeco
+    def get_status(self):
+        return self.__status
+    def get_idFuncionario(self):
+        return self.__idPessoa
+
+    def __str__(self):
+        return f"{self.__id} - {self.__descricao} - {self.__data_comeco} - {self.__status} - {self.__idPessoa}"
+    def to_dict(self):
+        return {"id": self.__id,"descrição": self.__descricao,"Começo": self.__data_comeco, "status": self.__status, "id de membros": self.__idPessoa}
+      
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            id=data['id'],
+            descricao=data['descriçao'],
+            email=data['data_comeco'],
+            cell=data['status'],
+            senha=data['idPessoa']
+        )
